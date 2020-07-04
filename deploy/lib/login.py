4353bd6ff0e7f3400d4facbcc6e34c33df1745f0
fulder <misad90@gmail.com>
2020-07-04 19:55:09 +0200

    Add login stack with cognito user pool

--- deploy/lib/login.py
+++ deploy/lib/login.py
@@ -1,5 +1,5 @@
 from aws_cdk import core
-from aws_cdk.aws_cognito import UserPool
+from aws_cdk.aws_cognito import UserPool, Mfa, MfaSecondFactor, UserVerificationConfig, VerificationEmailStyle
 
 
 class Login(core.Stack):
@@ -12,5 +12,13 @@ class Login(core.Stack):
         UserPool(
             self,
             "movio",
-            user_pool_name="movio-userpool"
+            user_pool_name="movio-userpool",
+            mfa=Mfa.OPTIONAL,
+            mfa_second_factor=MfaSecondFactor(otp=True, sms=False),
+            self_sign_up_enabled=False,
+            user_verification=UserVerificationConfig(
+                email_subject="Movio email verification",
+                email_body="Hello {username}, Thanks for signing up to movio! Verify your account by clicking on {##Verify Email##}",
+                email_style=VerificationEmailStyle.LINK
+                )
         )
