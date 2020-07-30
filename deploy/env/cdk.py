f85612178a6f21c71e759821c5229a12b3d31ed2
fulder <misad90@gmail.com>
2020-07-30 10:05:30 +0200

    Hardcode domain_name

--- deploy/env/cdk.py
+++ deploy/env/cdk.py
@@ -9,14 +9,11 @@ app = core.App()
 
 env = {"region": "eu-west-1"}
 
-domain_name = os.getenv("DOMAIN_NAME")
-if domain_name is None:
-    raise RuntimeError("Please set the DOMAIN_NAME environment variable")
-
 cert_arn = os.getenv("CERT_ARN")
 if cert_arn is None:
     raise RuntimeError("Please set the CERT_ARN environment variable")
 
+domain_name = "auth.moshan.tv"
 
 Login(app, "login", domain_name, cert_arn, env=env)
 
